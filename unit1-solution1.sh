6ae37d68f715bd4d4780fc0957a74fd1ef1a00db
student <lu0248@flinders.edu.au>
2019-03-22 18:03:12 +1030

    Unit1Exercise1 sumbission

---
+++ unit1-solution1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+mkdir -v -m 775 unit1exercise1
+cd unit1exercise1
+mkdir -v -p -m 352 enfahrer/zerhundtete/gelauftest
+mkdir -v -p -m 230 zerrennheit/antrauung/besetzkeit
+mkdir -v -p -m 044 enfahrer/zerhundtete/ausgekaesst
+mkdir -v -p -m 353 begehse/bekraukeit/angewarfkeit
+mkdir -v -p -m 400 enfahrer/zerhundtete/aufgehundtete
+mkdir -v -p -m 204 besprachs/ausfahrte/ausrauchheit
+mkdir -v -p -m 125 zerrennheit/antrauung/einsetzkeit
+chmod -v 405 zerrennheit/antrauung
+chmod -v 473 enfahrer/zerhundtete
+mkdir -v -p -m 504 enfahrer/angegehst
+chmod -v 234 besprachs/ausfahrte
+chmod -v 405 begehse/bekraukeit
+mkdir -v -m 267 aushundtest
+mkdir -v -m 725 zerpflumst
+mkdir -v -m 702 aufsitzung
+chmod -v 443 enfahrer
+chmod -v 201 zerrennheit
+chmod -v 440 besprachs
+chmod -v 440 begehse
+mkdir -v -m 072 aufgeschmeckheit

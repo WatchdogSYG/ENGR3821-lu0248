a5cb8a8be4d4160e556c5d456d98e153ada05429
lu0248 <lu0248@flinders.edu.au>
2019-03-24 15:24:06 +1030

    u1e2 submission

---
+++ unit1-solution2.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+mkdir -m 775 unit1exercise2
+cd unit1exercise2
+mkdir -p -m 760 angesinner/enrennt/zerhaltkeit
+sudo chown news angesinner/enrennt/zerhaltkeit
+mkdir -p -m 101 verfahren/aufgekraukeit/zerschmeckung
+sudo chown proxy verfahren/aufgekraukeit/zerschmeckung
+sudo chgrp audio verfahren/aufgekraukeit/zerschmeckung
+mkdir -p -m 764 besetzst/angesteht/antraute
+sudo chown mail besetzst/angesteht/antraute
+sudo chgrp voice besetzst/angesteht/antraute
+mkdir -p -m 657 angesinner/enrennt/angetraut
+sudo chown uucp angesinner/enrennt/angetraut
+sudo chgrp tape angesinner/enrennt/angetraut
+mkdir -p -m 547 angesinner/gegehtest/angelaufs
+sudo chgrp proxy angesinner/gegehtest/angelaufs
+mkdir -p -m 651 verfahren/ausgesinntest/verrenns
+sudo chown proxy verfahren/ausgesinntest/verrenns
+sudo chgrp cdrom verfahren/ausgesinntest/verrenns
+mkdir -p -m 720 verfahren/ausgesinntest/angeklettt
+sudo chown nobody verfahren/ausgesinntest/angeklettt
+chmod 070 besetzst/angesteht
+sudo chown games besetzst/angesteht
+sudo chgrp tape besetzst/angesteht
+chmod 620 angesinner/gegehtest
+sudo chown mail angesinner/gegehtest
+sudo chgrp tape angesinner/gegehtest
+chmod 204 verfahren/ausgesinntest
+sudo chown proxy verfahren/ausgesinntest
+sudo chgrp floppy verfahren/ausgesinntest
+chmod 745 verfahren/aufgekraukeit
+sudo chown uucp verfahren/aufgekraukeit
+sudo chgrp news verfahren/aufgekraukeit
+chmod 426 angesinner/enrennt
+sudo chown nobody angesinner/enrennt
+chmod 020 besetzst 
+sudo chown games besetzst
+sudo chgrp voice besetzst
+mkdir -m 331 ausgetrautete
+sudo chown news ausgetrautete
+sudo chgrp floppy ausgetrautete
+mkdir -m 460 angewarftest
+sudo chown uucp angewarftest
+sudo chgrp mail angewarftest
+mkdir -m 446 gesitzse 
+sudo chgrp dip gesitzse
+mkdir -m 103 aufgehundkeit
+sudo chown nobody aufgehundkeit
+sudo chgrp proxy aufgehundkeit
+mkdir -m 323 aushaltheit
+sudo chown games aushaltheit
+sudo chgrp news aushaltheit
+chmod 046 angesinner
+sudo chown lp angesinner
+sudo chgrp audio angesinner
+chmod 564 verfahren
+sudo chown news verfahren
+sudo chgrp floppy verfahren

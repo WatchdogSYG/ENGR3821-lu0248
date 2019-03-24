c1be4b0c44e2007ca2acc92a91f57c7ac4ed3f8d
lu0248 <lu0248@flinders.edu.au>
2019-03-24 18:58:58 +1030

    U1E3 Submission

---
+++ unit1-solution3.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+mkdir -m 775 unit1exercise3
+cd unit1exercise3
+sudo install -d -m 4267 -o nobody -g proxy aufsetzkeit/enhaltung/auftrittung
+cd aufsetzkeit
+sudo chown uucp:audio enhaltung
+sudo chmod 402 enhaltung
+sudo install -d -m 4762 -o lp -g proxy eintrittung/bepflumer
+cd eintrittung
+sudo install -d -m 772 -o games -g cdrom aufrauchtete
+cd ..
+sudo chown proxy:proxy eintrittung
+sudo chmod 271 eintrittung
+sudo install -d -m 4021 -o games -g cdrom anfahrtete/ausgekatzetest
+sudo install -d -m 660 -o nobody -g tape anfahrtete
+cd ..
+sudo install -d -m 4516 -o uucp -g mail enkaess/einwarfse/aufsinnung
+cd enkaess/einwarfse
+sudo install -d -m 503 -o student -g uucp anschmeckst
+cd ..
+sudo install -d -m 4261 -o mail -g cdrom einwarfse
+cd ..
+sudo install -d -m 4126 -o nobody -g mail aufgehen/zerpflumkeit/versitzer
+cd aufgehen
+sudo install -d -m 506 -o games -g voice zerpflumkeit
+cd ..
+sudo install -d -m 152 -o games -g tape ansteher
+sudo install -d -m 4626 -o games -g floppy ausgegehst
+sudo install -d -m 763 -o student -g tape anrabarbt
+sudo install -d -m 4620 -o proxy -g cdrom anwitzheit
+sudo install -d -m 4472 -o proxy -g proxy aufgehunder
+sudo chown news:voice enkaess
+sudo chmod 542 enkaess
+sudo chown games:floppy aufsetzkeit
+sudo chmod 4261 aufsetzkeit
+sudo chown student:audio aufgehen
+sudo chmod 131 aufgehen

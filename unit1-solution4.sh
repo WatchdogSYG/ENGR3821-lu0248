ed47f641c17ba4fff8e2016b2ea379fe46932923
lu0248 <48578684+lu0248@users.noreply.github.com>
2019-03-25 13:58:57 +1030

    U1E4 Submission

---
+++ unit1-solution4.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+mkdir -m 775 unit1exercise4
+cd unit1exercise4
+sudo install -v -d -m 236 -o student -g mail zersinntest/aufgerabarbheit/verspracher
+cd zersinntest/aufgerabarbheit
+sudo install -v -d -m 347 -o student -g proxy besetzen
+sudo install -v -d -m 2751 -o student -g fax enkraust
+cd ..
+sudo install -v -d -m 766 -o student -g voice aufgerabarbheit
+cd ..
+sudo install -v -d -m 540 -o student -g news berennte/ausgehaltkeit/angesprachung
+cd berennte
+sudo install -v -d -m 2435 -o student -g news ausgehaltkeit
+sudo install -v -d -m 2101 -o student -g floppy einkaeste/verklettst
+sudo chown -v student:tape einkaeste
+sudo chmod -v 457 einkaeste
+cd ..
+sudo install -v -d -m 2604 -o student -g proxy berennte
+sudo install -v -d -m 746 -o student -g cdrom eingehs/angewarfse/ausgegeht
+cd eingehs/angewarfse
+sudo install -v -d -m 373 -o student -g cdrom einlaufen
+cd ..
+sudo install -v -d -m 416 -o student -g audio angewarfse
+cd ..
+sudo install -v -d -m 2774 -o student -g audio eingehs
+sudo install -v -d -m 124 -o student -g audio aussetztest/angewitztest
+sudo chown -v student:dip aussetztest
+sudo chmod -v  147 aussetztest
+sudo install -v -d -m 106 -o student -g tape verhaltt
+sudo install -v -d -m 177 -o student -g proxy zersinntest
+sudo install -v -d -m 2513 -o student -g dip anhundung
+sudo install -v -d -m 216 -o student -g tape angerennst
+sudo install -v -d -m 2530 -o student -g voice angehunder

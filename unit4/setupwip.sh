d9751aa541854ca0b8c6e1cf5149ee27f1b512b8
lu0248 <48578684+lu0248@users.noreply.github.com>
2019-04-17 23:31:47 +0930

    Update setupwip.sh

--- unit4/setupwip.sh
+++ unit4/setupwip.sh
@@ -11,8 +11,20 @@ git clone https://github.com/MEGA65/mega65-core.git
 
 #install dependencies
 sudo apt install make
+sudo apt-get install gcc
+
+#python 2.7.10
+cd /usr/src
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar xzf Python-2.7.10.tgz
+cd Python-2.7.10
+./configure
+make altinstall
+python2.7 -V
+cd $PWD
+
+#fpgajtag
 sudo apt-get install libusb-1.0-0-dev
-
-
 git clone https://github.com/cambridgehackers/fpgajtag.git
 cd fpgajtag
+make

624b66b41d05d76d7fd96e60e613e734d2c49322
lu0248 <48578684+lu0248@users.noreply.github.com>
2019-04-17 23:45:07 +0930

    Update setupwip.sh

--- unit4/setupwip.sh
+++ unit4/setupwip.sh
@@ -3,20 +3,22 @@
 #(AMD64 version) virtualised on an Oracle Virtualbox VM
 #Version 6.0.4 r128413
 
-echo "----------------\nInstalling mega65-core from a clean Ubuntu 18.04\n----------------"
-echo "Installing GIT"
+echo "--------Installing mega65-core from a clean Ubuntu 18.04--------"
+echo "AUTO: Installing git and cloning"
 #install git and then clone into a new working directory
 sudo apt-get install git
 git clone https://github.com/MEGA65/mega65-core.git
+git checkout development
 
+echo "AUTO: Installing dependencies"
 #install dependencies
 sudo apt install make
 sudo apt-get install gcc
 
 #python 2.7.10
 cd /usr/src
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-tar xzf Python-2.7.10.tgz
+sudo wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+sudo tar xzf Python-2.7.10.tgz
 cd Python-2.7.10
 ./configure
 make altinstall

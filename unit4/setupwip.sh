31c6d83f886f4bdd612186ac8d63c5762916af5b
lu0248 <48578684+lu0248@users.noreply.github.com>
2019-04-18 12:54:43 +0930

    Update setupwip.sh
    up to step 4 on https://github.com/MEGA65/mega65-core/blob/master/docs/build.md

--- unit4/setupwip.sh
+++ unit4/setupwip.sh
@@ -3,24 +3,48 @@
 #(AMD64 version) virtualised on an Oracle Virtualbox VM
 #Version 6.0.4 r128413
 
-echo "--------Installing mega65-core from a clean Ubuntu 18.04--------"
-echo "AUTO: $PWD"
-echo "AUTO: Installing git and cloning"
+echo "--------Installing mega65-core onto a clean Ubuntu 18.04--------"
+echo "--------$PWD"
+echo "--------Installing git and cloning"
 #install git and then clone into a new working directory
 sudo apt-get -y install git
 git clone https://github.com/MEGA65/mega65-core.git
 git checkout development
 
-echo "AUTO: Installing dependencies"
+echo "--------Installing dependencies"
 #install dependencies
+echo "--------make"
 sudo apt install make
+echo "--------gcc"
 sudo apt-get -y install gcc
+
+echo "--------fpgajtag"
+#fpgajtag
+#libs required for compilation of fpgajtag and zlib
+sudo apt-get install -y libusb-1.0-0-dev
+cd ..
+#TO CLEANUP
+git clone https://github.com/cambridgehackers/fpgajtag.git
+cd fpgajtag/src
+#TO CLEANUP
+git clone https://github.com/madler/zlib
+sed -i 's|#include <zlib.h>|#include "zlib/zlib.h"|' util.c
+sudo apt-get install -y zlib1g-dev
+cd ..
+make
+sudo cp src/fpgajtag /usr/local/bin
+cd ..
+cd mega65-core
+echo "--------DONE fpgajtag"
 
 #python 2.7.10
 #instructions: https://tecadmin.net/install-python-2-7-on-ubuntu-and-linuxmint/
 sudo apt-get -y update
+echo "--------build-essential checkinstall"
 sudo apt-get -y install build-essential checkinstall
+echo "--------libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev"
 sudo apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+echo "--------Python 2.7.10"
 cd /usr/src
 sudo wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 sudo tar xzf Python-2.7.10.tgz
@@ -29,8 +53,3 @@ sudo ./configure
 sudo make altinstall
 cd $PWD
 
-#fpgajtag
-sudo apt-get -y install libusb-1.0-0-dev
-git clone https://github.com/cambridgehackers/fpgajtag.git
-cd fpgajtag
-make
